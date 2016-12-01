3a55e5b52af0cb8b2cf8380ba7eee283c58b8b9d
wongjenn <wongjenn@yahoo.com>
2016-12-01 18:13:30 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Dog
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each { |dog| puts dog.name }
+  end
+end
